b3033541d8e643ff585da2df31b5f487e17a16a5
nicolas-cardona <francisco@wecraftapps.com>
2023-12-13 19:36:50 +0100

    use of OsmdComponent in HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo-osmd.svg">
+    <h1>Open Sheet Music Display example with Vue.js 2</h1>
+    <select name="file" id="file" v-model="selectedSheetMusic">
+      <option disabled select value>Select your sheet music</option>
+      <option v-for="(item, index) in sheetMusicFiles" :key="index" :value="item">
+        {{ `${item.author}-${item.title}` }}
+      </option>
+    </select>
+    <OsmdComponent v-if="selectedSheetMusic"
+      :file="selectedSheetMusic.file"
+      :key="selectedSheetMusic.file">
+    </OsmdComponent>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import OsmdComponent from '@/components/OpenSheetMusicDisplay';
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
-  }
+    OsmdComponent,
+  },
+  data() {
+    return {
+      selectedSheetMusic: '',
+      sheetMusicFiles: [
+        {
+          author: 'Mozart',
+          title: 'An Chloe',
+          file: 'sheet-music-files/mozart-an_chloe.xml'
+        },
+        {
+          author: 'Mozart',
+          title: 'Das Veilchen',
+          file: 'sheet-music-files/mozart-das_veilchen.xml'
+        },
+      ]
+    }
+  },
 }
 </script>
